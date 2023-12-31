39c0725f124d3fa8ec3d049fea07cb75ec295bf0
Mayanksharma21 <mayank.sharmaa219@gmail.com>
2023-12-31 05:37:19 +0530

    Todo project Completed

--- todoapp/src/context/TodoContext.js
+++ todoapp/src/context/TodoContext.js
@@ -4,7 +4,7 @@ export const TodoContext = createContext({
     todos: [
         {
             id: 1,
-            todoMsg: "Todo Message",
+            todo: "Todo Message",
             completed: false
         }
     ],
